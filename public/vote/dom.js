bc6c02b6c5cf1176ddb22011ab97caf11810d343
Jorge Alonso <jalonsomerchan@gmail.com>
2026-05-13 18:40:53 +0200

    Expose top card DOM nodes

--- public/vote/dom.js
+++ public/vote/dom.js
@@ -14,6 +14,12 @@ export function getVoteNodes() {
     settingsModal: $('[data-settings-modal]'),
     nameForm: $('[data-name-form]'),
     nameInput: $('#voter-name-input'),
+    topCardContest: $('[data-top-card-contest]'),
+    topCardCopy: $('[data-top-card-copy]'),
+    topCardDownload: $('[data-top-card-download]'),
+    topCardFeedback: $('[data-top-card-feedback]'),
+    topCardLimits: document.querySelectorAll('[data-top-card-limit]'),
+    topCardPreview: $('[data-top-card-preview]'),
   };
 }
 
